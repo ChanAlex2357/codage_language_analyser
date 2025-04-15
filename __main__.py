5296ebfc76dc6ccd1c517357c277fddd661a8150
ChanAlex2357 <jacqueschanalex@gmail.com>
2025-04-15 14:46:08 +0300

    fonction test du prefix

--- __main__.py
+++ __main__.py
@@ -3,14 +3,15 @@ print("Ce code permet de déterminer si un langage L est un code ou non\n\n")
 
 L = ("00", "01", "110", "001")  # Le langage L à déterminer si c'est un code ou pas
 
+def test_prefix(L):
 # Exemple de test
-for i in range(len(L)):
-    for j in range(len(L)):
-        if i != j:
-            result = isPrefix(L[i], L[j])
-            if result == 1:
-                print(f"{L[i]} est un préfixe de {L[j]}")
-            elif result == 0:
-                print(f"{L[i]} est égal à {L[j]}")
-            else:
-                print(f"{L[i]} n'est pas un préfixe de {L[j]}")
+    for i in range(len(L)):
+        for j in range(len(L)):
+            if i != j:
+                result = isPrefix(L[i], L[j])
+                if result == 1:
+                    print(f"{L[i]} est un préfixe de {L[j]}")
+                elif result == 0:
+                    print(f"{L[i]} est égal à {L[j]}")
+                else:
+                    print(f"{L[i]} n'est pas un préfixe de {L[j]}")
