3a29ba9772f1d70f64b4ce01e9a98934211cd12b
ChanAlex2357 <jacqueschanalex@gmail.com>
2025-04-15 14:41:57 +0300

    init function

--- __main__.py
+++ __main__.py
@@ -1,3 +1,16 @@
 print("Ce code pemet de deteminer si un language L est un code ou non")
 
-L = ( "00", "01", "110", "001") # Le language L a determiner si c'est un language ou pas
+L = ("00", "01", "110", "001") # Le language L a determiner si c'est un language ou pas
+
+def isPrefix(u,v):
+    """
+        Determine si u est un prefixe de v
+        Params:
+            - u le mot a tester si c'est un prefixe ou non
+            - v le mot de test du prefixe
+        Returns:
+            - (-1) si ce n'est pas un prefixe
+            - (0) si u == v
+            - (1) si u est un prefixe de v
+    """
+    pass
