b208d232da078fb3a540f28c92398b4ffed3643b
ChanAlex2357 <jacqueschanalex@gmail.com>
2025-04-15 15:40:28 +0300

    rectification du residus et debug print

--- __main__.py
+++ __main__.py
@@ -1,5 +1,6 @@
 from codage import *
 print("Ce code permet de déterminer si un langage L est un code ou non\n\n")
 
-L = ("00", "01", "110", "001")  # Le langage L à déterminer si c'est un code ou pas
+L = ("000","010","011","01001")  # Le langage L à déterminer si c'est un code ou pas
 
+print("\nEst-ce un code ? > ", analyse_code_language(L))
--- codage/__init__.py
+++ codage/__init__.py
@@ -16,7 +16,7 @@ def isPrefix(u, v):
 def residuel(M, L):
     """
     Fonction qui trouve les résiduels de M par rapport à L.
-    Renvoie une liste de résiduels uniques.
+    Renvoie une liste de résiduels uniques (set).
     """
     residuels = set()  # valeurs uniques, pas de doublons
 
@@ -24,10 +24,13 @@ def residuel(M, L):
         for v in L:
             prefix_status = isPrefix(u, v)
             if prefix_status == 1:
-                residuels.add(u)  # u est un vrai préfixe de v
+                # u est un vrai préfixe de v => résiduel = suffixe de v après u
+                residuels.add(v[len(u):])
             elif prefix_status == 0:
-                residuels.add('')  # u == v
+                # u == v => résiduel = mot vide
+                residuels.add('')
     return residuels
+
 
 
 def analyse_code_language(L):
@@ -43,7 +46,9 @@ def analyse_code_language(L):
     # L[1] = residuel(L, L) - {''}
     residuels_1 = residuel(L, L)
     residuels_1.discard('')  # enlever les mots vides
+    print(f"L_1 : {residuels_1}")
     L_residuels.append(list(residuels_1))
+    
 
     n = 1
     while n >= 1:
@@ -52,11 +57,14 @@ def analyse_code_language(L):
         res2 = residuel(L_residuels[n], L)
         residuels_n1 = res1.union(res2)
 
+        print(f"L_{n+1} : {residuels_n1}")
         # Si l'ensemble contient '', alors ce n'est pas un code
         if '' in residuels_n1:
+            print(f"for n = {n} il y a un residu dans : {residuels_n1}")
             return False
 
-        residuels_1 = list(residuels_1)
+        residuels_n1 = list(residuels_n1)
+        
         # Si le pattern est déjà présent, on arrête
         if residuels_n1 in L_residuels:
             break
